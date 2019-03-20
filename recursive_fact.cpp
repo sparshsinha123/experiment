af48ea0ff13c1f606f75857f7e22303c6a48add0
Sparsh5008 <sparsh5008@gmail.com>
2019-03-20 12:54:44 -0700

    recursive fact added

---
+++ recursive_fact.cpp
@@ -0,0 +1,7 @@
+#include "recursive_fact.h"
+
+int recursive_fact(int n)
+{
+    if(n==0) return 1;
+    return n*fact(n-1);
+}
---
+++ recusive_fact.h
@@ -0,0 +1,5 @@
+#ifndef _recursivefun_
+#define _recursivefun_
+
+int recursive_fact(int n);
+#endif // _recursivefun_
